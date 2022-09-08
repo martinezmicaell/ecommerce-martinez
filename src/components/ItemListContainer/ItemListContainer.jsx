d621b8db4795361cc53a6a871a1cfe3a16678808
martinezmicaell <programacionmicael@gmail.com>
2022-09-08 12:25:32 -0300

    actualizando componentes

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ({greetings}) => {
+  return (
+    <h1>{greetings}</h1>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CartWidget from './CartWidget/CartWidget'
+const Navbar = () => {
+  return (
+    <>
+        <ul>
+            <li>Hello</li>
+            <li>Como</li>
+            <li>Estas</li>
+            <li>Bro</li>
+            <li>Fine?</li>
+
+            <CartWidget/>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
