c49b65340b7d8661c6a6cae8d10cb211fe7962a2
Micael Martinez <micaelmartinez@MacBook-Pro-de-Micael.local>
2022-09-12 19:24:30 -0300

    add ItemListContainer component in UI

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.scss'
 import './variables.module.scss'
 
 import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <ItemListContainer greetings="Bienvenidos a mi Tienda online!" />
         </div>
     );
 }
